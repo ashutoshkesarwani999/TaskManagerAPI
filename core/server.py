71cb37dc33d4b2992bfc4482b8a0f94878fd6c0d
= <ashutoshkesarwani999@gmail.com>
2024-11-16 20:46:30 +0000

    feat: Added fastapi server configuration

---
+++ core/server.py
@@ -0,0 +1,41 @@
+from typing import List
+
+from fastapi import Depends, FastAPI
+from fastapi.middleware import Middleware
+from fastapi.middleware.cors import CORSMiddleware
+
+from api import router
+from core.config import config
+
+
+def init_routers(app_: FastAPI) -> None:
+    app_.include_router(router)
+
+
+
+def make_middleware() -> List[Middleware]:
+    middleware = [
+        Middleware(
+            CORSMiddleware,
+            allow_origins=["*"],
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+        )
+    ]
+    return middleware
+
+
+def create_app() -> FastAPI:
+    app_ = FastAPI(
+        title="Task Manager API",
+        description="Create update delete and View your tasks",
+        version="1.0.0",
+        docs_url=None if config.ENVIRONMENT == "production" else "/docs",
+        middleware=make_middleware(),
+    )
+    init_routers(app_=app_)
+    return app_
+
+
+app = create_app()
