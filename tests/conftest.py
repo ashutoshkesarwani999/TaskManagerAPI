ebb9ead7763d35c02eb0c6e29cbd32cbd904a161
= <ashutoshkesarwani999@gmail.com>
2024-11-18 00:33:33 +0000

    fix: updated conftest to not drop tables after testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,8 @@
-import asyncio
-import os
-from typing import Generator
-
 import pytest
 import pytest_asyncio
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
-import core.database.transaction as transactional
 from app.models.task import Base
 from core.config import config
 
@@ -17,30 +12,11 @@ TEST_DATABASE_URL = config.POSTGRES_URL
 config.POSTGRES_URL = TEST_DATABASE_URL
 
 
-# @pytest.fixture(scope="session")
-# def event_loop(request) -> Generator:
-#     loop = asyncio.get_event_loop_policy().new_event_loop()
-#     yield loop
-#     loop.close()
-
-
 @pytest_asyncio.fixture(scope="function")
 async def db_session() -> AsyncSession:
     async_engine = create_async_engine(config.POSTGRES_URL)
     session = sessionmaker(async_engine, class_=AsyncSession, expire_on_commit=False)
 
-    # async with session() as s:
-    #     async with async_engine.begin() as conn:
-    #         await conn.run_sync(Base.metadata.create_all)
-
-    #     transactional.session = s
-    #     yield s
-
-    # async with async_engine.begin() as conn:
-    #     await conn.run_sync(Base.metadata.drop_all)
-    #     pass
-
-    # await async_engine.dispose()
     async with async_engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
@@ -50,7 +26,7 @@ async def db_session() -> AsyncSession:
         yield session
     finally:
         await session.close()
-        # Drop tables and cleanup
-        async with async_engine.begin() as conn:
-            await conn.run_sync(Base.metadata.drop_all)
+        # # Drop tables and cleanup
+        # async with async_engine.begin() as conn:
+        #     await conn.run_sync(Base.metadata.drop_all)
         await async_engine.dispose()
